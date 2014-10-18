d9c7b6f9130fb536c9f3da9c8299e6b7c2695443
derson2388 <273168148@qq.com>
2014-10-18 16:41:49 +0800

    Create package
    Craate network communicate/database/test modoule

---
+++ src/com/dersoncheng/test/UnitTestActivity.java
@@ -0,0 +1,13 @@
+package com.dersoncheng.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UnitTestActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}
